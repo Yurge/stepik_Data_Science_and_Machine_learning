4912b2b6547a3471a74b920a0c0c6b5393f825b8
Yury <vibornih@bk.ru>
2024-05-04 22:23:47 +0300

    added a file "csv" and started writing code

---
+++ machines/warships/warships.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+
+warships = pd.read_csv('/Users/yvvyds/PycharmProjects/stepik_Data_Science_and_Machine_learning/'
+                       'machines/warships/invasion.csv')
+
+print(warships)
